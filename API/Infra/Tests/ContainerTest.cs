cc32c084e971ff82475fe6c06bbf9d36dac1035a
ian <ianchute@hotmail.com>
2015-02-21 09:25:01 +0800

    First

---
+++ API/Infra/Tests/ContainerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StructureMap;
+
+namespace API.Infra.Tests
+{
+    [TestFixture]
+    public class ContainerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            ObjectFactory.Configure(ContainerConfiguration.Configure);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            ObjectFactory.Initialize();
+        }
+
+        [Test]
+        public void Test()
+        {
+            ObjectFactory.Container.AssertConfigurationIsValid();
+        }
+    }
+}
---
+++ API/Infra/Tests/MapperTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using NUnit.Framework;
+
+namespace API.Infra.Tests
+{
+    [TestFixture]
+    public class MapperTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Mapper.Initialize(MapperConfiguration.Configure);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Mapper.Reset();
+        }
+
+        [Test]
+        public void Test()
+        {
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
